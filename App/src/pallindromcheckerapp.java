7abbfa0b68b717db8f51793d2f4e82d2e2a0d7c0
lavitra661 <lavitra.kumar@gmail.com>
2026-02-27 16:05:03 +0530

    Welcome message

---
+++ App/src/pallindromcheckerapp.java
@@ -0,0 +1,11 @@
+public class pallindromcheckerapp {
+
+    public static void main(String[] args ){
+        System.out.println("welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println(" System initialized successfully");
+
+
+    }
+
+}
